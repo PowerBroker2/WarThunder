254816c25207dd837c6ece193fe9f9d16d98947c
PB2 <ltbergeron@gmail.com>
2019-06-09 17:49:58 -0400

    Added check to see if air battle is running

--- cpp/WTC.h
+++ cpp/WTC.h
@@ -1,38 +1,193 @@
-#pragma once
+#include "pch.h"
+#include "WTC.h"
 #include <fstream>
 #include <iostream>
-#include <urlmon.h>
-#include <string>
-#include <map>
-#include <wininet.h>
+#include <ctime>
 
 
 
 
-typedef std::map<std::string, std::string> dict;
+#pragma warning(disable : 4996)
 
 
 
 
-std::string replaceAll(std::string const& s, char const c, char const r);
-std::string removeAll(std::string const& s, char const c);
-template<typename T> void print(T value);
-void print(dict map);
-void print();
+WTC_telemetry myTelem;
+
+const int numKeys = 8;
+std::string basicKeys[numKeys] = { "IAS km/h",				// airspeed			km/h
+									"type",					// acft type		type
+									"altitude_hour",		// altitude			meters
+									"flaps %",				// flap position	%
+									"gear %",				// gear position	%
+									"compass",				// heading			degrees
+									"aviahorizon_pitch",	// pitch angle		degrees
+									"aviahorizon_roll" };	// roll angle		degrees
 
 
 
 
-class WTC_telemetry
+void datalog(std::string csvName, dict basic);
+std::string initializeCSV();
+void writeHeaderToCSV(std::string csvName);
+void writeToCSV(std::string csvName, std::string s);
+void writeCommaToCSV(std::string csvName);
+void writeNewlineToCSV(std::string csvName);
+
+
+
+
+int main()
 {
-public:
-	void begin();
-	dict getIndicators();
-	dict getState();
-	dict getBasic(dict indicators, dict state);
-	dict getWebContents(char webAddress[]);
+	dict indicators;
+	dict state;
+	dict combined;
+	dict basic;
+	dict::iterator it;
 
-private:
-	void htmlToString(char htmlFileName[], char htmlContentsBuff[], int buffLen);
-	dict stringToMap(std::string const& s);
-};
+	myTelem.begin();
+
+	std::string csvName = initializeCSV();
+	writeHeaderToCSV(csvName);
+
+	while (1)
+	{
+		indicators = myTelem.getIndicators();
+
+		it = indicators.find("valid");
+		if (it == indicators.end())
+		{
+			print((std::string)"Air battle not running");
+			continue;
+		}
+		else
+		{
+			if (it->second == "false")
+			{
+				print((std::string)"Air battle not running");
+				continue;
+			}
+		}
+
+		state = myTelem.getState();
+		basic = myTelem.getBasic(indicators, state);
+
+		datalog(csvName, basic);
+		print(basic);
+		print();
+	}
+
+	return 0;
+}
+
+
+
+
+void datalog(std::string csvName, dict map)
+{
+	dict::iterator it;
+
+	writeToCSV(csvName, "0");
+	writeCommaToCSV(csvName);
+	
+	for (int i = 0; i < numKeys; i++)
+	{
+		it = map.find(basicKeys[i]);
+
+		if (it != map.end())
+			writeToCSV(csvName, it->second);
+
+		writeCommaToCSV(csvName);
+	}
+
+	writeToCSV(csvName, "-");
+	writeNewlineToCSV(csvName);
+}
+
+
+
+
+std::string initializeCSV()
+{
+	time_t rawtime;
+	struct tm * timeinfo;
+	char buffer[80];
+
+	time(&rawtime);
+	timeinfo = localtime(&rawtime);
+
+	strftime(buffer, sizeof(buffer), "%d-%m-%Y %H:%M:%S", timeinfo);
+	std::string str(buffer);
+
+	std::string csvName;
+
+	csvName = "datalog_" + str + ".csv";
+	csvName = replaceAll(csvName, '-', '_');
+	csvName = replaceAll(csvName, ' ', '_');
+	csvName = replaceAll(csvName, ':', '_');
+
+	std::ofstream outFile;
+	outFile.open(csvName, std::ios::out | std::ios::app);
+	outFile.close();
+
+	return csvName;
+}
+
+
+
+
+void writeHeaderToCSV(std::string csvName)
+{
+	writeToCSV(csvName, "sampleTime");
+	writeCommaToCSV(csvName);
+
+	for (int i = 0; i < numKeys; i++)
+	{
+		writeToCSV(csvName, basicKeys[i]);
+		writeCommaToCSV(csvName);
+	}
+
+	writeToCSV(csvName, "comments");
+	writeNewlineToCSV(csvName);
+}
+
+
+
+
+void writeToCSV(std::string csvName, std::string s)
+{
+	std::ofstream outFile;
+	outFile.open(csvName, std::ios::out | std::ios::app);
+
+	for (auto it = s.begin(); it != s.end(); it++)
+		if ((*it != '\n') && (*it != ','))
+			outFile << *it;
+
+	outFile.close();
+}
+
+
+
+
+void writeCommaToCSV(std::string csvName)
+{
+	std::ofstream outFile;
+	outFile.open(csvName, std::ios::out | std::ios::app);
+
+	outFile << ',';
+
+	outFile.close();
+}
+
+
+
+
+void writeNewlineToCSV(std::string csvName)
+{
+	std::ofstream outFile;
+	outFile.open(csvName, std::ios::out | std::ios::app);
+
+	outFile << '\n';
+
+	outFile.close();
+}
