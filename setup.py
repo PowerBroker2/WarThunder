c21ea9abcc5272bc198f890c7b5e9afb41aaccee
PB2 <gitstuff2@gmail.com>
2019-12-15 00:07:17 -0500

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name         = 'WarThunder_Telemetry',
+    packages     = ['WarThunder_Telemetry'],
+    version      = '1.0.0',
+    description  = 'Python package used to access air vehicle telemetry while in War Thunder air battles',
+    author       = 'Power_Broker',
+    author_email = 'gitstuff2@gmail.com',
+    url          = 'https://github.com/PowerBroker2/WarThunder_Telemetry',
+    download_url = 'https://github.com/PowerBroker2/WarThunder_Telemetry/archive/1.0.0.tar.gz',
+    keywords     = ['War Thunder'],
+    classifiers  = [],
+)
