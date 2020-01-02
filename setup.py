5b07480bb8a1322cb4acf9b0920524f1b4ef7f22
PB2 <gitstuff2@gmail.com>
2020-01-02 15:34:57 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from setuptools import setup
 setup(
     name             = 'WarThunder',
     packages         = ['WarThunder'],
-    version          = '2.0.12',
+    version          = '2.0.13',
     description      = 'Python package used to access air vehicle telemetry while in War Thunder air battles',
     author           = 'Power_Broker',
     author_email     = 'gitstuff2@gmail.com',
     url              = 'https://github.com/PowerBroker2/WarThunder',
-    download_url     = 'https://github.com/PowerBroker2/WarThunder/archive/2.0.12.tar.gz',
+    download_url     = 'https://github.com/PowerBroker2/WarThunder/archive/2.0.13.tar.gz',
     keywords         = ['War Thunder'],
     classifiers      = [],
 )
