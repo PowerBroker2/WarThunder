01ae4e44e4008d7a73e8118523701de0ac97f85f
PB2 <gitstuff2@gmail.com>
2020-01-05 18:00:23 -0500

    More graceful error handling

--- WarThunder/general.py
+++ WarThunder/general.py
@@ -2,5 +2,8 @@ import os
 
 
 def get_version(game_path=r'C:\Program Files (x86)\Steam\steamapps\common\War Thunder'):
-    with open(os.path.join(game_path, r'content\pkg_main.ver')) as file:
-        return file.read()
+    try:
+        with open(os.path.join(game_path, r'content\pkg_main.ver')) as file:
+            return file.read()
+    except FileNotFoundError:
+        return '1.0.0'
