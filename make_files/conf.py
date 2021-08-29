b78ef1a566fc8b9ed77970898240717407f02c78
PB2 <gitstuff2@gmail.com>
2021-08-29 16:25:19 -0700

    Add files via upload

--- make_files/conf.py
+++ make_files/conf.py
@@ -30,8 +30,15 @@ release = '2.3.0'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = [ 'sphinx.ext.autodoc'
+extensions = [ 
+    'sphinx.ext.autodoc',
+    'sphinx.ext.napoleon',
+    'sphinx_autodoc_typehints'
 ]
+
+
+autoclass_content = 'both'
+atuodoc_member_order = 'bysource'
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -52,4 +59,4 @@ html_theme = 'classic'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = []
